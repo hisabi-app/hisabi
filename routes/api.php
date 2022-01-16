8f13c70700a99950d75544ac8a17c5bee9daacb9
Haneef Macbook Pro <haneef5k@gmail.com>
2022-01-16 23:11:18 +0400

    Added Basic API Endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,10 +17,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
+
+
+Route::get('/brand/create', function (Request $request) {
+    return Brand::get();
 });
 
 Route::get('/brands', function (Request $request) {
     return Brand::get();
+});
+
+Route::get('/categories', function (Request $request) {
+    return Category::get();
+});
+
+Route::get('/transactions', function (Request $request) {
+    return Transaction::with('brand')->get()->transform(function ($transaction) {
+        return [
+            'id' => $transaction->id,
+            'amount' => $transaction->amount,
+            'brand' => $transaction->brand->name,
+            'created_at' => $transaction->created_at,
+            'updated_at' => $transaction->updated_at,
+        ];
+    });
 });
