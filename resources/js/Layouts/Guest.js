27475f6dce2c501feba9a103468e15cdce501a33
Saleem Hadad <saleem.hadad@talabat.com>
2022-01-16 17:05:14 +0400

    💄 UI enhancement

--- resources/js/Layouts/Guest.js
+++ resources/js/Layouts/Guest.js
@@ -3,6 +3,8 @@ import React from 'react';
 export default function Guest({ children }) {
     return (
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+            <img className="h-20" src="/images/logo.png" />
+            
             <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
                 {children}
             </div>
