4df827cc79b1d57b3e4d404b13ef39ec29ad3496
Haneef Macbook Pro <haneef5k@gmail.com>
2022-01-16 22:33:43 +0400

    Added Brands API

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Brand;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/brands', function (Request $request) {
+    return Brand::get();
 });
