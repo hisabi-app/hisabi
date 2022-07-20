a07a9d8931640860e290c9d9fe32306eaef323a1
Haneef Macbook Pro <haneef5k@gmail.com>
2022-07-20 22:16:30 +0400

    Updated migration to accept default color value instead of nullable

--- database/migrations/2022_07_16_083805_add_color_column_in_table_categories.php
+++ database/migrations/2022_07_16_083805_add_color_column_in_table_categories.php
@@ -14,7 +14,7 @@ class AddColorColumnInTableCategories extends Migration
     public function up()
     {
         Schema::table('categories', function (Blueprint $table) {
-            $table->string('color')->after('type')->nullable();
+            $table->string('color')->after('type')->default('badge-gray');
         });
     }
 
