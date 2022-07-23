070c4d2b3df53de91bcd6cec51a83964c3db274b
Saleem Hadad <saleem.hadad@talabat.com>
2022-07-24 02:04:38 +0400

    update seeds

--- database/seeders/CategoryBrandSeeder.php
+++ database/seeders/CategoryBrandSeeder.php
@@ -14,34 +14,34 @@ class CategoryBrandSeeder extends Seeder
      */
     public function run()
     {
-        Category::create(['name' => 'Income', 'type' => Category::INCOME])
+        Category::create(['name' => 'Income', 'type' => Category::INCOME, 'color' => 'red'])
             ->brands()
             ->create(['name' => 'Salary']);
 
-        Category::create(['name' => 'Housing', 'type' => Category::EXPENSES])
+        Category::create(['name' => 'Housing', 'type' => Category::EXPENSES, 'color' => 'blue'])
             ->brands()
             ->create(['name' => 'House Rent']);
 
-        Category::create(['name' => 'Groceries', 'type' => Category::EXPENSES])
+        Category::create(['name' => 'Groceries', 'type' => Category::EXPENSES, 'color' => 'green'])
             ->brands()
             ->createMany([
                 ['name' => 'LULU'],
                 ['name' => 'CARREFOUR'],
             ]);
 
-        Category::create(['name' => 'Utilities', 'type' => Category::EXPENSES])
+        Category::create(['name' => 'Utilities', 'type' => Category::EXPENSES, 'color' => 'orange'])
             ->brands()
             ->createMany([
                 ['name' => 'Smart Dubai'],
             ]);
 
-        Category::create(['name' => 'Transportation', 'type' => Category::EXPENSES])
+        Category::create(['name' => 'Transportation', 'type' => Category::EXPENSES, 'color' => 'purple'])
             ->brands()
             ->createMany([
                 ['name' => 'ENOC'],
             ]);
 
-        Category::create(['name' => 'Shopping', 'type' => Category::EXPENSES])
+        Category::create(['name' => 'Shopping', 'type' => Category::EXPENSES, 'color' => 'pink'])
             ->brands()
             ->createMany([
                 ['name' => 'IKEA'],
@@ -49,13 +49,13 @@ class CategoryBrandSeeder extends Seeder
                 ['name' => 'MCDONALDS'],
             ]);
 
-        Category::create(['name' => 'Support', 'type' => Category::EXPENSES])
+        Category::create(['name' => 'Support', 'type' => Category::EXPENSES, 'color' => 'indigo'])
             ->brands()
             ->createMany([
                 ['name' => 'Family Support'],
             ]);
 
-        Category::create(['name' => 'Debt', 'type' => Category::EXPENSES])
+        Category::create(['name' => 'Debt', 'type' => Category::EXPENSES, 'color' => 'gray'])
             ->brands()
             ->createMany([
                 ['name' => 'Debt'],
