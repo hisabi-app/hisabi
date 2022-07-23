e35d2e1d62f06a87505f038a62922226052a9297
Haneef Macbook Pro <haneef5k@gmail.com>
2022-07-23 21:46:31 +0400

    Updated test cases

--- tests/Feature/CategoriesTest.php
+++ tests/Feature/CategoriesTest.php
@@ -40,7 +40,7 @@ class CategoriesTest extends TestCase
                             'id' => $category->id,
                             'name' => $category->name,
                             'type' => $category->type,
-                            'color' => $category->color
+                            'color' => 'badge-gray'
                         ],
                     ],
                     "paginatorInfo" => [
