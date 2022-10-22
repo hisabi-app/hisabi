2ab1c173770d23e8b61b4151d54e987d5836bd08
Saleem Hadad <saleem.hadad@talabat.com>
2022-10-22 14:06:36 +0400

    cleanup code

--- routes/web.php
+++ routes/web.php
@@ -8,17 +8,6 @@ use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\SmsController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::redirect('/', '/dashboard');
 
 Route::middleware(['auth'])->group(function () {
