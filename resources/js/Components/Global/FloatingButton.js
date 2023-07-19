8e5e394216dae34dfc3646119765a876f40131f5
Saleem Hadad <saleem.hadad@talabat.com>
2023-07-19 23:03:38 +0400

    Add floating button component for FinanceGPT

---
+++ resources/js/Components/Global/FloatingButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { CogIcon } from '@heroicons/react/outline';
+
+const FloatingButton = () => {
+  return (
+    <div className="fixed bottom-4 left-0 w-full flex justify-center">
+      <button
+        className='bg-teal-500 hover:bg-teal-600 text-white p-2 rounded-full shadow-xl flex items-center'
+      >
+        <CogIcon className="w-6 h-6 animate-spin-slow" />
+        <p>FinanceGPT</p>
+      </button>
+    </div>
+  );
+};
+
+export default FloatingButton;
