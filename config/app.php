155867c6270f89ee9c94cfd78210ed04502c49e2
Haneef Ansari-Macbook-Pro14 <10325179+ahaneef29@users.noreply.github.com>
2023-07-31 00:00:00 +0400

    Upgrade to Laravel 10

--- config/app.php
+++ config/app.php
@@ -164,6 +164,7 @@ return [
 
         /*
          * Package Service Providers...
+         * Package Service Providers...
          */
 
         /*
@@ -229,7 +230,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
     ],
 
 ];
--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Auth;
+namespace Tests\Feature;
 
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
