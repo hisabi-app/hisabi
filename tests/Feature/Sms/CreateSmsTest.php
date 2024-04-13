b896d853ae07c2ee438944626e5f0bc31e767e66
Saleem Hadad <saleem.hadad@talabat.com>
2024-04-14 01:11:43 +0400

    accept default date from client

--- tests/Feature/Sms/CreateSmsTest.php
+++ tests/Feature/Sms/CreateSmsTest.php
@@ -35,4 +35,30 @@ class CreateSmsTest extends TestCase
 
         $this->assertCount(1, Sms::all());
     }
+
+    /** @test */
+    public function it_process_created_at_if_provided()
+    {
+        $this->graphQL(/** @lang GraphQL */ '
+            mutation {
+                createSms(body: """Payment of AED 38.7 to someBrand with Credit Card ending 9048. Avl Cr. Limit is AED 53,750.64.""" created_at: """2022-05-01""") {
+                    id
+                    body
+                    transaction_id
+                }
+            }
+            ')->assertJson([
+            'data' => [
+                'createSms' => [
+                    [
+                        "id" => 1,
+                        "body" => "Payment of AED 38.7 to someBrand with Credit Card ending 9048. Avl Cr. Limit is AED 53,750.64.",
+                        "transaction_id" => 1,
+                    ]
+                ],
+            ],
+        ]);
+
+        $this->assertEquals("2022-05-01", Sms::first()->transaction->created_at->format('Y-m-d'));
+    }
 }
