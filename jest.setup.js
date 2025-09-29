d094d4aafca21e308257d3325937b09a2f8a046f
Saleem Hadad <saleem_ha@hotmail.com>
2025-09-29 14:43:52 +0400

    fix tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,23 @@
 import { server } from './resources/js/mocks/server.js'
 
+// Mock location object for API client
+Object.defineProperty(window, 'location', {
+  value: {
+    origin: 'http://localhost:3000',
+    href: 'http://localhost:3000',
+    protocol: 'http:',
+    hostname: 'localhost',
+    port: '3000',
+    pathname: '/',
+    search: '',
+    hash: ''
+  },
+  writable: true
+});
+
+// Mock global location for Node environment
+global.location = window.location;
+
 beforeAll(() => server.listen())
 afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
